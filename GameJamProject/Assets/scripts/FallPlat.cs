7949f4ffa179da6c5190e75bf2e66d319a7c71e7
Anthos973 <anthony.sagnard@ynov.com>
2016-11-30 08:56:55 +0100

    JoueurScript

---
+++ GameJamProject/Assets/scripts/FallPlat.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallPlat: MonoBehaviour
+{
+    public Transform platform;
+    public float fallDelay = 1f;
+    public bool hasPlayed = false;
+
+    private Rigidbody2D rb2d;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasPlayed == false)
+        {
+            hasPlayed = true;       
+        }
+
+        rb2d.isKinematic = false;    }
+
+}
---
+++ GameJamProject/Assets/scripts/Falling.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformFall : MonoBehaviour
+{
+
+    public float fallDelay = 1f;
+
+
+    private Rigidbody2D rb2d;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+   
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+
+    void Update(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+}
